3468f826c2112e2af50b4556c6d742cb16d265b2
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2018-06-04 11:13:02 -0700

    WIP; transX idea

--- index.js
+++ index.js
@@ -4,33 +4,52 @@ class RobotEyes {
   // }
 
   _createKeyframes ({
+    tgtTranXVal = 0,
     tgtTranYVal = 0,
     tgtRotVal = 0,
     enteredOffset = 1/3,
     exitingOffset = 2/3,
   } = {}) {
     return [
-      {transform: `translateY(0px) rotate(0deg)`, offset: 0.0},
-      {transform: `translateY(${tgtTranYVal}px) rotate(${tgtRotVal}deg)`, offset: enteredOffset},
-      {transform: `translateY(${tgtTranYVal}px) rotate(${tgtRotVal}deg)`, offset: exitingOffset},
-      {transform: `translateY(0px) rotate(0deg)`, offset: 1.0},
+      {transform: `translateX(0px) translateY(0px) rotate(0deg)`, offset: 0.0},
+      {transform: `translateX(${tgtTranXVal}px) translateY(${tgtTranYVal}px) rotate(${tgtRotVal}deg)`, offset: enteredOffset},
+      {transform: `translateX(${tgtTranXVal}px) translateY(${tgtTranYVal}px) rotate(${tgtRotVal}deg)`, offset: exitingOffset},
+      {transform: `translateX(0px) translateY(0px) rotate(0deg)`, offset: 1.0},
     ];
   }
 
   makeFacialExpression({
     type = '',
-    durationMs = 2000,
-    enterDurationMs = 500,
-    exitDurationMs = 500,
+    // level = 3,  // 1: min, 5: max
+    durationMs = 1000,
+    enterDurationMs = 75,
+    exitDurationMs = 75,
   }) {
+    const options = {
+      duration: durationMs,
+    }
+    const upperLeftEyelid = document.querySelector('.left .eyelid.upper');
+    const upperRightEyelid = document.querySelector('.right .eyelid.upper');
+    const lowerLeftEyelid = document.querySelector('.left .eyelid.lower');
+    const lowerRightEyelid = document.querySelector('.right .eyelid.lower');
     switch(type) {
-      case 'sad':
-        const upperLeftEyelid = document.querySelector('.left .eyelid.upper');
-        const upperRightEyelid = document.querySelector('.right .eyelid.upper');
+      case 'happy':
+        lowerLeftEyelid.animate(this._createKeyframes({
+          tgtTranYVal: 80,
+          tgtTranYVal: 80,
+          tgtRotVal: -20,
+          enteredOffset: enterDurationMs / durationMs,
+          exitingOffset: 1 - (exitDurationMs / durationMs),
+        }), options);
+        lowerRightEyelid.animate(this._createKeyframes({
+          tgtTranYVal: 80,
+          tgtRotVal: 20,
+          enteredOffset: enterDurationMs / durationMs,
+          exitingOffset: 1 - (exitDurationMs / durationMs),
+        }), options);
+        break;
 
-        const options = {
-          duration: durationMs,
-        }
+      case 'sad':
         upperLeftEyelid.animate(this._createKeyframes({
           tgtTranYVal: 80,
           tgtRotVal: -20,
@@ -44,14 +63,30 @@ class RobotEyes {
           exitingOffset: 1 - (exitDurationMs / durationMs),
         }), options);
         break;
+
+      case 'mad':
+        upperLeftEyelid.animate(this._createKeyframes({
+          tgtTranYVal: 50,
+          tgtRotVal: 30,
+          enteredOffset: enterDurationMs / durationMs,
+          exitingOffset: 1 - (exitDurationMs / durationMs),
+        }), options);
+        upperRightEyelid.animate(this._createKeyframes({
+          tgtTranYVal: 50,
+          tgtRotVal: -30,
+          enteredOffset: enterDurationMs / durationMs,
+          exitingOffset: 1 - (exitDurationMs / durationMs),
+        }), options);
+        break;
+
       default:
         console.warn(`Invalid input type: ${type}`);
     }
   }
 
-  happy() {
-    console.log('implement happy');
-  }
+  // happy() {
+  //   console.log('implement happy');
+  // }
 
   // sad({
   //   durationMs = 2000,
@@ -65,7 +100,7 @@ class RobotEyes {
     const upperEyelids = [...document.querySelectorAll('.eyelid.upper')];
     const lowerEyelids = [...document.querySelectorAll('.eyelid.lower')];
     const options = {
-      duration: 2000,
+      duration: 1000,
       iterations: 1,
     }
     upperEyelids.map(eyelid => {
@@ -74,27 +109,6 @@ class RobotEyes {
     lowerEyelids.map(eyelid => {
       eyelid.animate(this._createKeyframes({tgtTranYVal: -50}), options);
     });
-  }
-
-  angry() {
-    console.log('implement happy');
-      // if ([...eyelid.classList].indexOf("lower") >= 0) {
-      //   return;
-      // }
-
-      // const transYDir = '-';
-      // const rotDir = ([...eyelid.parentElement.classList].indexOf("left") >= 0) ? '' : '-';
-
-      // eyelid.animate([
-      //   // keyframes
-      //   {transform: `translateY(0) rotate(0)`, offset: 0.0},
-      //   {transform: `translateY(50px) rotate(${rotDir}30deg)`, offset: 0.2},
-      //   {transform: `translateY(50px) rotate(${rotDir}30deg)`, offset: 1.0},
-      // ], {
-      //   // timing options
-      //   duration: 5000,
-      //   iterations: Infinity
-      // });
   }
 
   confuse() {
