5f0a7c75b40eb9ca5d9d7ffec4bb9f16d6c61b81
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2018-06-04 09:09:23 -0700

    WIP

--- index.js
+++ index.js
@@ -1,50 +1,121 @@
-const blinkRandomly = (timeoutMs) => {
-  setTimeout(() => {
-    [...document.getElementsByClassName('eye')].map((eye) => {
+class RobotEyes {
+  // constructor() {
+
+  // }
+
+  // sad() {
+
+  //   // const targetRotVal = 20;
+
+  // const offset2 = 500 / durationMs ;
+  // document.querySelector('.left .eyelid.upper').animate([
+  //   {transform: `translateY(0px) rotate(0)`, offset: 0.0},
+  //   {transform: `translateY(80px) rotate(-20deg)`, offset: 0.1},
+  //   {transform: `translateY(80px) rotate(-20deg)`, offset: 0.9},
+  //   {transform: `translateY(0px) rotate(0deg)`, offset: 1.0},
+  // ], {
+  //   duration: 2000,
+  //   iterations: 1,
+  // });
+  // document.querySelector('.right .eyelid.upper').animate([
+  //   {transform: `translateY(0) rotate(0)`, offset: 0.0},
+  //   {transform: `translateY(80px) rotate(20deg)`, offset: 0.1},
+  //   {transform: `translateY(80px) rotate(20deg)`, offset: 0.9},
+  //   {transform: `translateY(0px) rotate(0deg)`, offset: 1.0},
+  // ], {
+  //   duration: 2000,
+  //   iterations: 1,
+  // });
+  // [...document.querySelectorAll('.eye')].map(eye => eye.animate([
+  //   {transform: `translateY(0px)`, offset: 0.0},
+  //   {transform: `translateY(20px)`, offset: 0.1},
+  //   {transform: `translateY(20px)`, offset: 0.9},
+  //   {transform: `translateY(0px)`, offset: 1.0},
+  // ], {
+  //   duration: 2000,
+  //   iterations: 1,
+  // }));
+  // }
+
+  // makeFacialExpression() {
+
+  // }
+
+  blink({
+    duration = 150,  // in ms
+  } = {}) {
+    return [...document.getElementsByClassName('eye')].map((eye) => {
       eye.animate([
-        {transform: 'rotateX(0deg)', offset: 0.0},
-        {transform: 'rotateX(90deg)', offset: 0.5},
-        {transform: 'rotateX(0deg)', offset: 1.0},
+        {transform: 'rotateX(0deg)'},
+        {transform: 'rotateX(90deg)'},
+        {transform: 'rotateX(0deg)'},
       ], {
-        duration: 150,  // in ms
+        duration,
         iterations: 1,
       });
     });
-    blinkRandomly((Math.random() * 6 + 2) * 1000);
-  }, timeoutMs);
+  }
+}
+
+const robotEyes = new RobotEyes();
+
+// Blink
+
+let blinkHandle = null;
+
+const startBlinking = (maxIntervalMs = 5000) => {
+  const blinkRandomly = (timeoutMs) => {
+    blinkHandle = setTimeout(() => {
+      robotEyes.blink();
+      blinkRandomly(Math.random() * maxIntervalMs);
+    }, timeoutMs);
+  }
+  blinkRandomly(Math.random() * maxIntervalMs);
 };
 
-blinkRandomly((Math.random() * 6 + 2) * 1000);
+const stopBlinking = () => {
+  clearTimeout(blinkHandle);
+}
 
 /** Sad **/
 
-document.querySelector('.left .eyelid.upper').animate([
-  {transform: `translateY(0px) rotate(0)`, offset: 0.0},
-  {transform: `translateY(80px) rotate(-20deg)`, offset: 0.1},
-  {transform: `translateY(80px) rotate(-20deg)`, offset: 0.9},
-  {transform: `translateY(0px) rotate(0deg)`, offset: 1.0},
-], {
-  duration: 2000,
-  iterations: 1,
-});
-document.querySelector('.right .eyelid.upper').animate([
-  {transform: `translateY(0) rotate(0)`, offset: 0.0},
-  {transform: `translateY(80px) rotate(20deg)`, offset: 0.1},
-  {transform: `translateY(80px) rotate(20deg)`, offset: 0.9},
-  {transform: `translateY(0px) rotate(0deg)`, offset: 1.0},
-], {
-  duration: 2000,
-  iterations: 1,
-});
-[...document.querySelectorAll('.eye')].map(eye => eye.animate([
-  {transform: `translateY(0px)`, offset: 0.0},
-  {transform: `translateY(20px)`, offset: 0.1},
-  {transform: `translateY(20px)`, offset: 0.9},
-  {transform: `translateY(0px)`, offset: 1.0},
-], {
-  duration: 2000,
-  iterations: 1,
-}));
+// const blinkRandomly = (level, durationMs) => {
+//   // () => {
+
+//   // }
+//   const getKeyframes = (maxTransY, maxRot, offsetInPct, offsetOutPct) => {
+//     // throw error if duration is too short
+//   };
+
+//   const offset2 = 500 / durationMs ;
+//   document.querySelector('.left .eyelid.upper').animate([
+//     {transform: `translateY(0px) rotate(0)`, offset: 0.0},
+//     {transform: `translateY(80px) rotate(-20deg)`, offset: 0.1},
+//     {transform: `translateY(80px) rotate(-20deg)`, offset: 0.9},
+//     {transform: `translateY(0px) rotate(0deg)`, offset: 1.0},
+//   ], {
+//     duration: 2000,
+//     iterations: 1,
+//   });
+//   document.querySelector('.right .eyelid.upper').animate([
+//     {transform: `translateY(0) rotate(0)`, offset: 0.0},
+//     {transform: `translateY(80px) rotate(20deg)`, offset: 0.1},
+//     {transform: `translateY(80px) rotate(20deg)`, offset: 0.9},
+//     {transform: `translateY(0px) rotate(0deg)`, offset: 1.0},
+//   ], {
+//     duration: 2000,
+//     iterations: 1,
+//   });
+//   [...document.querySelectorAll('.eye')].map(eye => eye.animate([
+//     {transform: `translateY(0px)`, offset: 0.0},
+//     {transform: `translateY(20px)`, offset: 0.1},
+//     {transform: `translateY(20px)`, offset: 0.9},
+//     {transform: `translateY(0px)`, offset: 1.0},
+//   ], {
+//     duration: 2000,
+//     iterations: 1,
+//   }));
+// }
 
 /** Focus **/
 // [...document.getElementsByClassName('eyelid')].map((eyelid) => {
